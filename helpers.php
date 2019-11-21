c97fb01b5b9bc5753444a1fdd82d13e47cb61b86
Yoël <yoel.teixeira@gmail.com>
2019-11-21 11:51:19 +0100

    Possibilité injection de variables dans vues

--- helpers.php
+++ helpers.php
@@ -19,7 +19,12 @@ function value_input_type($name) {
     elseif (isset($_SESSION["$name"])) echo htmlspecialchars($_SESSION["$name"]);
 }
 
-function load_view($name) {
+function load_view($name, $data = []) {
+
+    foreach($data as $k => $v) {
+        ${$k} = $v;
+    }
+
     ob_start();
     require_once(app_path('View/'.$name.'.php'));
     $view = ob_get_contents();
